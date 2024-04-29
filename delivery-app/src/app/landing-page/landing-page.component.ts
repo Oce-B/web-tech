742f3875d189e0ef9056f6f35e49a4ea83a09219
Oce-B <oceane.brigitte-raffin@eleve.isep.fr>
2024-04-29 17:21:59 +0200

    project structure chan

--- delivery-app/src/app/landing-page/landing-page.component.ts
+++ delivery-app/src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RestaurantRatingComponent } from '../retaurant/restaurant-rating/restaurant-rating.component';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [RestaurantRatingComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
---
+++ delivery-app/src/app/retaurant/restaurant-type/restaurant-type.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-type',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurant-type.component.html',
+  styleUrl: './restaurant-type.component.scss'
+})
+export class RestaurantTypeComponent {
+
+}
